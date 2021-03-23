ca73a46d70a323b9f1f119394f31f0f3d6132e64
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-23 15:02:39 -0500

    Move position of scroll-spy function of window.onload

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ $(window).on('load', function () {
     // makes sure that whole site is loaded
     $('#preloader-gif, #preloader').fadeOut(5000, function () {});
 });
+/* =========================================
+            Simple-Scroll-Spy
+    ============================================ */
+window.onload = function() {
+    scrollSpy('#header__list', {
+        sectionClass: '.scrollspy',
+        menuActiveTarget: '.header__link',
+        offset: 100
+    });
+}
 
 $(document).ready(function() {
     /*===================================================================================
@@ -35,10 +45,8 @@ $(document).ready(function() {
     $('.header__logo, .header__link, #hero__button').on('click', function (event) {
         if (this.hash !== '') {
             event.preventDefault();
-
             //store hash in a new variable
             let hash = this.hash;
-
             $('html, body').animate({
                     scrollTop: $(hash).offset().top
                 },
@@ -55,12 +63,15 @@ $(document).ready(function() {
     =====================================================================*/
     let list = document.getElementById('header__list');
     let links = list.getElementsByClassName('header__link');
+
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener('click', function () {
+            console.log(`link - ${i}`)
             let current = list.getElementsByClassName('active');
-            current[0].className = current[0].className.replace(' active', ' ');
+            console.log(links[i+1]);
+            current[0].className = links[i-1].className.replace('.active', '');
             this.className += ' active';
-            console.log(current[0].classList);
+            console.log(`this -> ${this.classList}`);
         })
         // console.log(list);
     }
@@ -75,14 +86,5 @@ $(document).ready(function() {
         current[0].className = current[0].className.replace(' active', ' ');
         link.className += ' active';
     });
-    /* =========================================
-            Simple-Scroll-Spy
-    ============================================ */
-    window.onload = function() {
-        scrollSpy('#header__list', {
-            sectionClass: '.scrollspy',
-            menuActiveTarget: '.header__link',
-            offset: 100
-        });
-    }
+
 })
