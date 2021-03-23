ca324f7feb714b98294df68a80812f4552daff74
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-23 10:47:10 -0500

    Modify: enable closing of menu with clicking of header__link

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
 $(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger, .header__menu').toggleClass('active')
-        $('body').toggleClass('lock')
+
+
+
+    /*===================================================================================
+    toggleMenu Function - toggles the header__burger, header__menu, and body with classes
+    * active or lock
+    ======================================================================================*/
+    function toggleMenu() {
+        $('.header__burger').toggleClass('active');
+        $('.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    }
+    $('.header__burger').on('click tap', function () {
+        toggleMenu();
+    });
+
+    /*===================================================================
+    * clicking on a link or anywhere in the header__menu closes the menu and
+    * enables scroll on the body
+    =====================================================================*/
+    $('.header__list, .header__link').on('click', function() {
+        $('.header__burger').removeClass('active');
+        $('.header__menu').removeClass('active');
+        $('body').removeClass('lock');
     })
+    /*===================================================================
+    *
+    =====================================================================*/
+
+
+
+
+
+
+
+    /* =========================================
+            Simple-Scroll-Spy
+    ============================================ */
+    window.onload = function() {
+        scrollSpy('#header__list', {
+            sectionClass: '.scrollspy',
+            menuActiveTarget: '.header__link',
+            offset: 100
+        });
+    }
+
 })
